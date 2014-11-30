356f681852929fb96289d9da349a331cc298dda6
John Brewer <gzroms@hotmail.com>
2014-11-30 12:50:34 -0500

    Revert "Derp"
    This reverts commit 7d61d38ffb7e646b5fbf073893c786283551d0e2.

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -227,7 +227,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         }
         mAirplaneModeOn = new ToggleAction(
                 R.drawable.ic_lock_airplane_mode,
-                R.string.global_actions_toggle_airplane_mode,
+                R.drawable.ic_lock_airplane_mode_off,
                 R.drawable.ic_lock_airplane_mode_off_dark,
                 R.string.global_actions_airplane_mode_on_status,
                 R.string.global_actions_airplane_mode_off_status) {
